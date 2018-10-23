504c38c8212ae9b8d5a2edb92dfd1295be057094
Moggelicious <Moggelicious@hotmail.com>
2018-10-23 14:25:17 +0200

    shadow

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ session_start();
 </head>
 <body>
     
-<nav class="navbar navbar-expand-sm navbar-light navbar-fixed-top navbar-custom">
+<nav class="navbar navbar-expand-sm navbar-light navbar-fixed-top navbar-custom shadow rounded">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
