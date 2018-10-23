4e2b00d19ab48e285f9b945937222e0f9c5759aa
Moggelicious <Moggelicious@hotmail.com>
2018-10-23 15:43:46 +0200

    new files navbar.php and head.php

---
+++ includes/navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-sm navbar-light navbar-fixed-top navbar-custom shadow rounded">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <h1 class="navbar-brand m-auto">Le Boutique</h1>
+  <button class="btn btn-sm align-middle btn-dark ml-auto mr-2 order-sm-last" type="button">
+    <span class="fa fa-shopping-cart" aria-hidden="true">
+  </span></button>
+
+  <div class="collapse navbar-collapse" order-first-md id="navbarToggler">
+    <ul class="navbar-nav ml-auto mt-0 mt-lg-0">
+
+        <?php
+            if(!isset($_SESSION['user'])): ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                </li>
+        <?php else : ?>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Logout</a>
+        </li>
+        <?php endif; ?>
+    </ul>
+  </div>
+</nav>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -4,58 +4,9 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Charmonman" rel="stylesheet">
-    <link rel="stylesheet" media="all" type="text/css" href="css/style.css">
-    <title>Document</title>
-</head>
+<?php include 'includes/head.php'; ?>
 <body>
-    
-<nav class="navbar navbar-expand-sm navbar-light navbar-fixed-top navbar-custom shadow rounded">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <h1 class="navbar-brand m-auto">Le Boutique</h1>
-  <button class="btn btn-sm align-middle btn-dark ml-auto mr-2 order-sm-last" type="button">
-    <span class="fa fa-shopping-cart" aria-hidden="true">
-  </span></button>
-
-  <div class="collapse navbar-collapse" order-first-md id="navbarToggler">
-    <ul class="navbar-nav ml-auto mt-0 mt-lg-0">
-
-        <?php
-    if(!isset($_SESSION['user'])): ?>
-    <li class="nav-item">
-    <a class="nav-link" href="#">Login</a>
-    </li>
-
-        <li class="nav-item">
-        <a class="nav-link" href="#">Register</a>
-        </li>
-    <?php
-    else :
-     ?>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Logout</a>
-      </li>
-      <?php endif; ?>
-    </ul>
-  </div>
-</nav>
-
-
-
-
-
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+ <?php 
+    include 'includes/navbar.php'; ?>
 </body>
 </html>
