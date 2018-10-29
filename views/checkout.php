a5541371536f819b5e75dbdddb420c5146e0b911
Moggelicious <Moggelicious@hotmail.com>
2018-10-29 10:12:25 +0100

    setting up checkout

--- views/checkout.php
+++ views/checkout.php
@@ -2,13 +2,15 @@
 session_start();
 ?>
 
-<?=
-
-
-
-      $_SESSION["id"],
-       $_SESSION["username"],
-       $_SESSION["product_name"],
-       $_SESSION["amount"],
-       $_SESSION["unit_price"]
-        ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include ('../includes/head.php');
+echo '<style>';
+include '../css/style.css';
+echo '</style>'; ?>
+<body>
+ <?php 
+    include ('../includes/navbar.php');
+    include ('../includes/products_from_db.php')?>
+</body>
+</html>
